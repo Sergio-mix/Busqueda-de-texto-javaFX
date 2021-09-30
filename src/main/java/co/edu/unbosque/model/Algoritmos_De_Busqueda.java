3b3dc19b793b365f2c21d75c83ed17199c374c21
alejo <alejo.80.123@hotmail.com>
2021-09-30 15:56:53 -0500

    Primer commit

---
+++ src/main/java/co/edu/unbosque/model/Algoritmos_De_Busqueda.java
@@ -0,0 +1,20 @@
+package co.edu.unbosque.model;
+
+public class Algoritmos_De_Busqueda {
+
+    public Algoritmos_De_Busqueda() {
+
+    }
+
+    public static String kMP(String text, String buscar) {
+        return null;
+    }
+
+    public static String bM(String text, String buscar) {
+        return null;
+    }
+
+    public static String boyer_Moore(String text, String buscar) {
+        return null;
+    }
+}
