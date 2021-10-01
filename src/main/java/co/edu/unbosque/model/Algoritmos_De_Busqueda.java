0dba9f858c73ae5170c4bee72865339506a08a07
alejo <alejo.80.123@hotmail.com>
2021-09-30 21:55:23 -0500

    commit

--- src/main/java/co/edu/unbosque/model/Algoritmos_De_Busqueda.java
+++ src/main/java/co/edu/unbosque/model/Algoritmos_De_Busqueda.java
@@ -1,5 +1,6 @@
 package co.edu.unbosque.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Algoritmos_De_Busqueda {
@@ -8,11 +9,34 @@ public class Algoritmos_De_Busqueda {
 
     }
 
-    public static List<Integer[]> kMP(String text, String buscar) {
+    public static List<String> basic(String text, String buscar) {
+        List<String> list = new ArrayList<>();
+        int s = buscar.length();
+        String subString;
+
+        for (int i = 0; i < text.length(); i++) {
+            int z = i + s;
+
+            if (i + s <= text.length()) {
+                subString = text.substring(i, z);
+                if (subString.equals(buscar)) {
+                    list.add(i + " " + z);
+                }
+            } else {
+                subString = text.substring(text.length() - s, text.length());
+                if (subString.equals(buscar)) {
+                    list.add(text.length() - s + " " + text.length());
+                }
+            }
+        }
+        return list;
+    }
+
+    public static List<String> kMP(String text, String buscar) {
         return null;
     }
 
-    public static List<Integer[]> bM(String text, String buscar) {
+    public static List<String> bM(String text, String buscar) {
         return null;
     }
 
